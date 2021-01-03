e233dc92beeee5ec7fb81efb091e7d29a08556b0
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2021-01-04 04:04:51 +0900

    # commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import {TLPage} from "./pages/TLPage";
+import {MyPage} from "./pages/MyPage";
+import {MainPage} from "./pages/MainPage";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    return (
+        <div className="App">
+            <ul>
+                <li><NavLink exact to="/">MainPage</NavLink></li>
+                <li><NavLink to="/timeline">TimeLine</NavLink></li>
+                <li><NavLink to="/mypage">MyPage</NavLink></li>
+            </ul>
+            <Switch>
+                <Route exact path="/"><MainPage /></Route>
+                <Route path="/timeline"><TLPage /></Route>
+                <Route path="/mypage"><MyPage /></Route>
+            </Switch>
+        </div>
+    );
+}
+ReactDom.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
